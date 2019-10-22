b763ce877f636a5491b996d0641b223c4b9e608d
Sixto Luis Alcoba Banqueri <sixto.alcobabanqueri@epfl.ch>
2019-10-22 12:40:25 +0200

    using 3 cameras on 3 screens

---
+++ Cameras2Screens/Assets/ScreenSplitter.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSplitter : MonoBehaviour
+{
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < Display.displays.Length; i++) { 
+            //Activate display at that size
+            Display.displays[i].Activate(1920, 1080, 60);
+
+            //Full Screen
+            Screen.SetResolution(1920, 1080, true);
+        }
+        //  Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+
+    }
+
+
+}
